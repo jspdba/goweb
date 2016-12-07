d20be7632206db14fd452b02eca97da7356f94d9
jspdba <jspdba@163.com>
2016-12-07 18:18:22 +0800

    go fmt

--- src/web/controllers/linkController.go
+++ src/web/controllers/linkController.go
@@ -1,9 +1,9 @@
 package controllers
 
 import (
+	"github.com/PuerkitoBio/goquery"
 	"github.com/astaxie/beego"
 	"strings"
-	"github.com/PuerkitoBio/goquery"
 	"web/models"
 )
 
@@ -12,69 +12,73 @@ type LinkController struct {
 }
 
 type Data struct {
-	Title string	`json:"title"`
-	Description string	`json:"description"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
 type JsonObj struct {
-	Code int	`json:"code"`
-	Msg string	`json:"msg"`
-	Data *Data	`json:"data"`
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data *Data  `json:"data"`
 }
 
-func(this *LinkController) URLMapping()  {
-	this.Mapping("/link/edit/:id([0-9]+)",this.Edit)
-	this.Mapping("/link/save",this.Save)
-	this.Mapping("/link/delete/:id([0-9]+)",this.Delete)
-	this.Mapping("/link/list",this.List)
-	this.Mapping("/link/info",this.Info)
+func (this *LinkController) URLMapping() {
+	this.Mapping("/link/edit/:id([0-9]+)", this.Edit)
+	this.Mapping("/link/save", this.Save)
+	this.Mapping("/link/delete/:id([0-9]+)", this.Delete)
+	this.Mapping("/link/list", this.List)
+	this.Mapping("/link/info", this.Info)
 }
 
 // @router /link/edit/:id([0-9]{0,}) [get]
-func(this *LinkController) Edit(){
-	this.TplName="link/edit.tpl"
+func (this *LinkController) Edit() {
+	this.TplName = "link/edit.tpl"
 }
+
 // @router /link/save [post]
-func(this *LinkController) Save(){
+func (this *LinkController) Save() {
 	link := models.Link{}
 	if err := this.ParseForm(&link); err != nil {
 		beego.Error(err)
 	}
-	tags:=this.GetString("Tags.Name")
-	models.LinkReadOrCreate(&link,tags)
-	this.Redirect("/link/list",302)
+	tags := this.GetString("Tags.Name")
+	models.LinkReadOrCreate(&link, tags)
+	this.Redirect("/link/list", 302)
 }
+
 // @router /link/delete/:id
-func(this *LinkController) Delete(){
+func (this *LinkController) Delete() {
 }
+
 // @router /link/list
-func(this *LinkController) List(){
-	this.TplName="link/list.tpl"
+func (this *LinkController) List() {
+	this.TplName = "link/list.tpl"
 }
+
 // @router /link/info
-func(this *LinkController) Info(){
-	url := strings.Trim(this.GetString("url"),"")
-	if url==""{
-		this.Data["json"]=&JsonObj{Code:1,Msg:"参数错误，url=nil"}
-	}else{
-		if !strings.HasPrefix(url,"http://") && !strings.HasPrefix(url,"https://"){
-			url = "http://"+url
+func (this *LinkController) Info() {
+	url := strings.Trim(this.GetString("url"), "")
+	if url == "" {
+		this.Data["json"] = &JsonObj{Code: 1, Msg: "参数错误，url=nil"}
+	} else {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
 		}
 		beego.Info(url)
-		this.Data["json"] = JsonObj{Code:0,Data:getUrlInfo(url)}
+		this.Data["json"] = JsonObj{Code: 0, Data: getUrlInfo(url)}
 	}
 	this.ServeJSON()
 }
 
-func getUrlInfo(url string) (data *Data){
+func getUrlInfo(url string) (data *Data) {
 	doc, err := goquery.NewDocument(url)
-	if err!=nil{
+	if err != nil {
 		beego.Error(err)
 		return &Data{}
 	}
-	if doc!=nil{
-		title:=doc.Find("title").Text()
-		content,_:=doc.Find("meta[name=description]").Attr("content")
-		data = &Data{title,content}
+	if doc != nil {
+		title := doc.Find("title").Text()
+		content, _ := doc.Find("meta[name=description]").Attr("content")
+		data = &Data{title, content}
 	}
 	return data
 }
--- src/web/models/models.go
+++ src/web/models/models.go
@@ -1,70 +1,71 @@
 package models
 
 import (
-	"time"
-	"github.com/astaxie/beego/orm"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 	"strings"
+	"time"
 )
+
 //收藏的链接
 type Link struct {
-	Id int			`orm:"auto"`
-	Url string		`orm:"unique;type(text)"`
-	Title string		`orm:"null;type(text)"`
-	Show bool		`orm:"default(true)"` //显示?1:0
-	Description string	`orm:"null;type(text)"`
-	CreateDate time.Time	`orm:"auto_now_add;type(datetime)"`
-	ModifyDate time.Time	`orm:"auto_now;type(datetime)"`
-	Tags []*Tag 		`orm:"null;rel(m2m);on_delete(do_nothing)"`
+	Id          int       `orm:"auto"`
+	Url         string    `orm:"unique;type(text)"`
+	Title       string    `orm:"null;type(text)"`
+	Show        bool      `orm:"default(true)"` //显示?1:0
+	Description string    `orm:"null;type(text)"`
+	CreateDate  time.Time `orm:"auto_now_add;type(datetime)"`
+	ModifyDate  time.Time `orm:"auto_now;type(datetime)"`
+	Tags        []*Tag    `orm:"null;rel(m2m);on_delete(do_nothing)"`
 }
 
 //标签
 type Tag struct {
-	Id int			`orm:"auto"`
-	Name string		`orm:"unique;type(text)"`
-	CreateDate time.Time	`orm:"auto_now_add;type(datetime)"`
-	Description string 	`orm:"null;type(text)"`
-	Link []*Link 		`orm:"null;reverse(many);on_delete(do_nothing)"`
+	Id          int       `orm:"auto"`
+	Name        string    `orm:"unique;type(text)"`
+	CreateDate  time.Time `orm:"auto_now_add;type(datetime)"`
+	Description string    `orm:"null;type(text)"`
+	Link        []*Link   `orm:"null;reverse(many);on_delete(do_nothing)"`
 }
 
-func TagReadOrCreate(name string) (tag *Tag){
-	if name!=""{
+func TagReadOrCreate(name string) (tag *Tag) {
+	if name != "" {
 		o := orm.NewOrm()
-		tag=new(Tag)
-		tag.Name=name
+		tag = new(Tag)
+		tag.Name = name
 		if _, id, err := o.ReadOrCreate(tag, "Name"); err == nil {
-			tag.Id=int(id)
-		}else{
+			tag.Id = int(id)
+		} else {
 			beego.Error(err)
 		}
 	}
 	return tag
 }
-func LinkReadOrCreate(link *Link,tags string) {
+func LinkReadOrCreate(link *Link, tags string) {
 	o := orm.NewOrm()
 
 	//save tags to db
-	if tags!=""{
-		tagArr:=strings.Split(tags,",")
-		link.Tags=make([]*Tag,len(tagArr))
-		for index,name := range tagArr{
-			tag:=TagReadOrCreate(name)
-			if tag!=nil{
-				link.Tags[index]=tag
+	if tags != "" {
+		tagArr := strings.Split(tags, ",")
+		link.Tags = make([]*Tag, len(tagArr))
+		for index, name := range tagArr {
+			tag := TagReadOrCreate(name)
+			if tag != nil {
+				link.Tags[index] = tag
 			}
 		}
 	}
 
 	// 三个返回参数依次为：是否新创建的，对象Id值，错误
 	if _, _, err := o.ReadOrCreate(link, "Url"); err == nil {
-		if len(link.Tags)>0{
-			beego.Info("Link.Tags size =",len(link.Tags))
+		if len(link.Tags) > 0 {
+			beego.Info("Link.Tags size =", len(link.Tags))
 			m2m := o.QueryM2M(link, "Tags")
-			if _,err := m2m.Add(link.Tags); err!=nil{
+			if _, err := m2m.Add(link.Tags); err != nil {
 				beego.Error(err)
 			}
 		}
-	}else{
+	} else {
 		beego.Error(err)
 	}
 }
@@ -73,5 +74,5 @@ func init() {
 	// 需要在init中注册定义的model
 	//maxIdle := 30
 	//maxConn := 30
-	orm.RegisterModel(new(Link),new(Tag))
+	orm.RegisterModel(new(Link), new(Tag))
 }
